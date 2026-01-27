3e5ee488ffb4c2c9453201c37bfd0af1c37936fc
TharsoRech <tharso_rech@hotmail.com>
2026-01-26 22:16:48 -0300

    add auto login

--- GetYourPlaceApp/Managers/AuthManager.swift
+++ GetYourPlaceApp/Managers/AuthManager.swift
@@ -10,7 +10,6 @@ class AuthManager {
     private let profileKey = "user_profile_cache"
 
     init() {
-        // AUTO-LOGIN LOGIC: Check if a profile exists in cache
         if let savedProfile = CacheManager.shared.load(forKey: profileKey, as: UserProfile.self) {
             self.currentUser = savedProfile
             self.isAuthenticated = true
@@ -18,18 +17,17 @@ class AuthManager {
     }
 
     func login(userProfile: UserProfile) {
-            withAnimation(.spring()) {
-                self.currentUser = userProfile
-                self.isAuthenticated = true
-                CacheManager.shared.save(userProfile, forKey: cacheKey)
-            }
+        withAnimation(.spring()) {
+            self.currentUser = userProfile
+            self.isAuthenticated = true
+            CacheManager.shared.save(userProfile, forKey: profileKey)
         }
+    }
 
     func logout() {
         withAnimation(.spring()) {
             self.isAuthenticated = false
             self.currentUser = nil
-            // Clear the cache
             CacheManager.shared.remove(forKey: profileKey)
         }
     }
